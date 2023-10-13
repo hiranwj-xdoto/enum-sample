76288ed88b33dbeb596de3cfe3f19150dac532dd
hiranwj <hiranwj.connect@gmail.com>
2023-10-13 11:13:34 +0530

    Initial commit

---
+++ src/main/java/com/hiranwj/enumsampleproject/EnumSampleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hiranwj.enumsampleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnumSampleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnumSampleProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hiranwj/enumsampleproject/EnumSampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiranwj.enumsampleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnumSampleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
